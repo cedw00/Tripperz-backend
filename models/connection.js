0218a5c0e336f76ba1387eeaeee26f02477d7a55
Cedric Chang <cedric.chang00@gmail.com>
2023-12-22 09:24:53 +0100

    Changed connection string to database

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = process.env.CONNECTION_STRING;
+const connectionString = 'mongodb+srv://admin:Shaktifa15@cluster0.zmkrfaw.mongodb.net/Tripperz';
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log('Database connected'))
